62694dbbc4588dafc0ef3f46ac0f22cd4d8d3b10
christian reinaldo <christian19782013@gmail.com>
2022-08-29 16:44:07 -0500

    docs antes de dar la prac mod 4
    docs antes de dar la prac mod 4

---
+++ IPYNB_TODOS/prueba.py
@@ -0,0 +1,16 @@
+#respondeme a la siguiente pregunta ¿la exactitud siempre es una buena métrica para  evaluar el desempeño de un modelo de clasificacion?
+# -*- coding: utf-8 -*-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
